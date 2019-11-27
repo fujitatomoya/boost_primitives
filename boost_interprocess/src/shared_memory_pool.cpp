70acf8304bb9c6c730267c46eea31fd2e1558766
AdolfoMartinez <adolfomartinez@eprosima.com>
2019-11-27 07:52:18 +0100

    added a test case with new operator

--- boost_interprocess/src/shared_memory_pool.cpp
+++ boost_interprocess/src/shared_memory_pool.cpp
@@ -6,17 +6,19 @@
 #include <ctime>
 #include <cstring>
 
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::nanoseconds nanoseconds;
+
+Clock::time_point t0, t1;
+
+using namespace boost::interprocess;
+
 #define SIZE_8MB (8*1024*1024)
 #define SIZE_4MB (4*1024*1024)
 
-int main (int argc, char *argv[])
+void test_shm()
 {
-  typedef std::chrono::high_resolution_clock Clock;
-  typedef std::chrono::nanoseconds nanoseconds;
-
-  Clock::time_point t0, t1;
-
-  using namespace boost::interprocess;
+  printf("--- test with shm ---\n");
 
   //Remove shared memory on construction and destruction
   struct shm_remove
@@ -33,6 +35,7 @@ int main (int argc, char *argv[])
   void * shptr = segment.allocate(SIZE_4MB);
   printf("whole memory size is %ld byte, the reset is %ld\n", free_memory, segment.get_free_memory());
 
+  uint64_t time_sum = 0;
   nanoseconds ns_diff = std::chrono::duration_cast<nanoseconds>(t1 - t0);
   for (int i = 0; i < 10; i++) {
     t0 = Clock::now();
@@ -40,8 +43,41 @@ int main (int argc, char *argv[])
     std::memset(shptr, 0, SIZE_4MB);
     t1 = Clock::now();
     ns_diff = std::chrono::duration_cast<nanoseconds>(t1 - t0);
+    time_sum += ns_diff.count();
     printf("%10ld nano seconds [%d iteration]\n", ns_diff.count(), i);
   }
+  printf("avg time = %.3f(ns)\n",time_sum / 10.0);
+}
+
+void test_new()
+{
+  printf("--- test with new ---\n");
+
+  //Allocate a portion of the segment (raw memory)
+  uint8_t* pool = new uint8_t[SIZE_8MB];
+  void* shptr = pool;
+
+  uint64_t time_sum = 0;
+  nanoseconds ns_diff = std::chrono::duration_cast<nanoseconds>(t1 - t0);
+  for (int i = 0; i < 10; i++) {
+    t0 = Clock::now();
+    // insert function here.
+    std::memset(shptr, 0, SIZE_4MB);
+    t1 = Clock::now();
+    ns_diff = std::chrono::duration_cast<nanoseconds>(t1 - t0);
+    time_sum += ns_diff.count();
+    printf("%10ld nano seconds [%d iteration]\n", ns_diff.count(), i);
+  }
+
+  printf("avg time = %.3f(ns)\n",time_sum / 10.0);
+
+  delete [] pool;
+}
+
+int main (int argc, char *argv[])
+{
+  test_new();
+  test_shm();
 
   return EXIT_SUCCESS;
 }
